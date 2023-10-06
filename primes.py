ff5e9b3929959b29309f30de3e3afd7f332a3650
AleksandraBlomberg <146368482+AleksandraBlomberg@users.noreply.github.com>
2023-10-06 15:29:23 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    List = []
+    if number_of_primes<=0:
+        raise ValueError("Value must be non-negative or 0")
+    else:
+        number = 2
+        while len(List)<number_of_primes:
+            count=2
+            flag = True
+            while flag:
+                if count == number:
+                    List.append(number)
+                    flag = False
+                if number % count != 0:
+                    count= count+1
+                else:
+                    number= number+1
+                    count = 2
+    return List
